3980e3efe1f89ae0c21edc42202f5f262747f3a1
Oleksandr Meteiko <oleksandr.meteiko@sap.com>
2023-08-17 15:08:20 +0200

    Bump dep

--- main.go
+++ main.go
@@ -1,10 +1,31 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+
 	println("Ba dum, tss!")
 	println("Ba dum, tss!")
 	println("Ba dum, tss!")
 	println("Ba dum, tss!")
 
 	println("For a release")
+
+	if a() && b() {
+		fmt.Println("a && b")
+	}
+}
+
+func a() bool {
+	fmt.Println("a")
+
+	return false
+}
+
+func b() bool {
+	fmt.Println("b")
+
+	return true
 }
